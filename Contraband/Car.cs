a96fa782dba62c4b3d2b2fd30a841e0a326f6ce9
Filip-Ericsson <filip.ericsson@elev.ga.ntig.se>
2020-01-30 14:50:49 +0100

    push

--- Contraband/Car.cs
+++ Contraband/Car.cs
@@ -16,44 +16,40 @@ namespace Contraband
 
         public bool Examine(int contrabandAmount)
         {
-            if (alreadyChecked == false)
+
+
+
+            if (contrabandAmount == 0)
             {
-                alreadyChecked = true;
-                if (contrabandAmount == 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    foundContraband = generator.Next(1, 10);
-                    if (foundContraband > 8 && contrabandAmount == 1)
-                    {
-                        return true;
-                    }
-                    else if (foundContraband > 6 && contrabandAmount == 2)
-                    {
-                        return true;
-                    }
-                    else if (foundContraband > 4 && contrabandAmount == 3)
-                    {
-                        return true;
-                    }
-                    else if (foundContraband > 2 && contrabandAmount == 4)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
+                return false;
             }
             else
             {
-                Console.WriteLine("This car is already Checked");
-                return false;
+                foundContraband = generator.Next(1, 10);
+                if (foundContraband > 8 && contrabandAmount == 1)
+                {
+                    return true;
+                }
+                else if (foundContraband > 6 && contrabandAmount == 2)
+                {
+                    return true;
+                }
+                else if (foundContraband > 4 && contrabandAmount == 3)
+                {
+                    return true;
+                }
+                else if (foundContraband > 2 && contrabandAmount == 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            
+
+
+
         }
     }
 }
--- Contraband/Program.cs
+++ Contraband/Program.cs
@@ -10,6 +10,61 @@ namespace Contraband
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            Console.WriteLine("Hur många bilar? Skriv ett nummer");
+            string userInput = "";
+            int amountOfCars = 0;
+            bool correctInput = false;
+            
+            Random generator = new Random();
+            
+            while (correctInput == false)
+            {
+                userInput = Console.ReadLine();
+                correctInput = userInput.All(char.IsDigit);
+
+
+                if (correctInput == true)
+                {
+                    amountOfCars = int.Parse(userInput);
+
+                }
+                else
+                {
+                    Console.WriteLine("Felaktig input, måste vara ett tal och större än 0!");
+                }
+
+            }
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+            for (int i = 0; i < amountOfCars; i++)
+            {
+                
+                if (generator.Next(0, 1) ==1)
+                {
+                    cars.Add(i.ToString(), new CleanCar());
+                }
+                else
+                {
+                    cars.Add(i.ToString(), new ContrabandCar());
+                }
+
+            }
+            for (int i = 0; i < amountOfCars; i++)
+            {
+                if (car.Examine(cars[Console.ReadLine()].contrabandAmount)== true)
+                {
+                    Console.WriteLine("Has contraband ");
+                }
+                else
+                {
+                    Console.WriteLine("all good");
+                }
+                
+            }
+            
+
+           
+            Console.ReadLine();
         }
     }
 }
