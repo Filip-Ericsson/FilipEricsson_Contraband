f161f6411d87c6099a7006b8fd8b523568dd149c
Filip-Ericsson <filip.ericsson@elev.ga.ntig.se>
2020-01-30 14:11:28 +0100

    arbetat på car

--- Contraband/Car.cs
+++ Contraband/Car.cs
@@ -12,11 +12,48 @@ namespace Contraband
         public int contrabandAmount = 0;
         public bool alreadyChecked;
         public Random generator = new Random();
+        int foundContraband = 0;
 
-
-        public bool Examine()
+        public bool Examine(int contrabandAmount)
         {
-
+            if (alreadyChecked == false)
+            {
+                alreadyChecked = true;
+                if (contrabandAmount == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    foundContraband = generator.Next(1, 10);
+                    if (foundContraband > 8 && contrabandAmount == 1)
+                    {
+                        return true;
+                    }
+                    else if (foundContraband > 6 && contrabandAmount == 2)
+                    {
+                        return true;
+                    }
+                    else if (foundContraband > 4 && contrabandAmount == 3)
+                    {
+                        return true;
+                    }
+                    else if (foundContraband > 2 && contrabandAmount == 4)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("This car is already Checked");
+                return false;
+            }
+            
         }
     }
 }
